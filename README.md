477fdbc7481ac7d140db969f0d6c79ea8ed467ab
Penaud Loïc <l.penaud@zaclys.net>
2017-09-17 23:30:37 +0200

    Add fallback to svg <object>

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## TODO
  - [ ] Rapetisser image profil
- - [ ] Réfléchir à comment afficher la liste des compétences
-    - [ ] Annimation svg
-    - [ ] Rajout texte d'explication logo (svg)
+ - [x] Réfléchir à comment afficher la liste des compétences
+ - [ ] Annimation svg
+ - [x] Rajout texte d'explication logo (svg)
+ - [ ] Rajout d'anchor ?
  - [ ] Rajouter les logos des réseaux sociaux
  - [ ] Réfléchir à où placer la div de "Centre d'intérêt"
- - [ ] Fallback image vectoriel
+ - [x] Fallback image vectoriel
+ - [ ] Réfléchir au responsive
--- js/skill.js
+++ js/skill.js
@@ -1,29 +1,62 @@
 window.onload = function () {
-    var skillSvg =  document.getElementById('object-skill').contentDocument.getElementById('skill-svg');
-    var skillSvgs = skillSvg.querySelectorAll('.skill-svg');
+    var objSvg = document.getElementById('object-skill');
+
+    var displaySkill = function (id) {
+        let ids = id.split('-');
+        let title = ids[ids.length - 1];
+
+        if (listSkill[title]) {
+            skillDiv.innerHTML = listSkill[title];
+        } else {
+            loadSkill(title);
+        }
+    }
+    
+
+    if (objSvg.contentDocument !== null) {
+        var skillSvg = objSvg.contentDocument.getElementById('skill-svg');
+        var skillSvgs = skillSvg.querySelectorAll('.skill-svg');
+
+        for (let i = 0; i < skillSvgs.length; i++) {
+            skillSvgs[i].onmouseover = function () {
+                displaySkill(this.id);
+            };
+        }
+
+    } else {
+        var skillList = document.querySelector('#object-skill > .list-group');
+
+        skillList.onclick = function (evt) {
+            var elements = document.querySelectorAll('#object-skill .list-group-item');
+
+            displaySkill(evt.target.id);
+
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.remove('active');
+            }
+
+            evt.target.classList.add('active');
+        };
+    }
 
     var defaultSkillDiv = document.getElementById('default-skill-div');
     var skillDiv = document.getElementById('skill-div');
+    var listSkill = {};
 
     function loadSkill(title) {
+
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 skillDiv.innerHTML = this.responseText;
                 defaultSkillDiv.style = "display: none";
+                listSkill[title] = this.responseText;
             }
         };
 
-        xhttp.open('GET', "html/skill/" + title + '.html');
+        xhttp.open('GET', "ajax-html/skill/" + title + '.html');
         xhttp.send();
-    }
-
-    for (let i = 0; i < skillSvgs.length; i++) {
-        skillSvgs[i].onmouseover = function (evt) {
-            let id = this.id.split('-');
-            loadSkill(id[id.length - 1]);
-        }
     }
 
 }
