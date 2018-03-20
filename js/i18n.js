26abc29397f3aa3ec8a3d32bdd85cb96b0b9b3c6
Loïc Penaud <l.penaud@zaclys.net>
2018-03-20 16:33:01 +0100

    Translate all titles

--- js/i18n.js
+++ js/i18n.js
@@ -1,7 +1,56 @@
-const selectAllDataText = function () {
-  const elements = Array.of(document.querySelectorAll('[data-text]'));
-  for (let i = elements.length; i > -1; i--) {
-    const element = elements[i];
-    console.log(element);
-  }
+const ajaxGetPromise = function (url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
+}
+
+const ajaxGetJSONPromise = function (url) {
+  return new Promise((resolve, reject) => {
+    ajaxGetPromise(url)
+      .then(data => resolve(JSON.parse(data)))
+      .catch(err => reject(err));
+  });
+}
+
+const getLanguage = function () {
+  return navigator.language.split('-')[0];
+}
+
+const translateAllDataText = function (language) {
+  const elements = document.querySelectorAll('[data-text]');
+  ajaxGetJSONPromise(window.location + 'i18n/' + language + '.json')
+    .then(data => {
+      for (let i = elements.length - 1; i > -1; i--) {
+        const element = elements[i]
+        const attributes = element.attributes;
+        for (let j = attributes.length; j > -1; j--) {
+          const attr = attributes[j];
+          if (attr && attr.name === 'data-text') {
+            if (element.tagName === "IMG") element.alt = data[attr.value];
+            else element.textContent = data[attr.value];
+          }
+        }
+      }
+    })
+    .catch(() => {
+      translateAllDataText('en');
+    });
 }
