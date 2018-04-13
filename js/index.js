421448281c24ee9f77297122bdb7f308f4614a60
vbellec <vincent.bellec29280@gmail.com>
2018-04-13 10:56:14 +0200

    First commit fork

--- js/index.js
+++ js/index.js
@@ -1,21 +1,15 @@
 window.onload = function () {
-  const bornDate = new Date(1998, 2, 23);
+  const bornDate = new Date(1998, 9, 19);
   const dateDiff = Date.now() - bornDate.getTime();
 
-  translateAll(getLanguage()).then(language => {
-    const selectLanguages = this.document.querySelector('#select-languages');
+  translateAll('fr').then(language => {
     let optionLanguages = availableLanguages.filter(lang => lang.value !== language);
     optionLanguages.push(availableLanguages.find(lang => lang.value === language));
     for (let i = optionLanguages.length - 1; i > -1 ; i--) {
       const opt = this.document.createElement('OPTION'); //optionLanguages[i]
       opt.value = optionLanguages[i].value;
       opt.textContent = optionLanguages[i].text;
-      selectLanguages.appendChild(opt);
     }
-
-    selectLanguages.addEventListener('change', evt => {
-      translateAll(evt.target.value);
-    });
   });
 
   this.document.querySelector('#span-years-old').textContent = (new Date(dateDiff)).getUTCFullYear() - 1970;
